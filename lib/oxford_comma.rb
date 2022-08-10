31c15ccafaba85f0ff2f4900f5a7bb58b3615c7d
Tyrone <tyrone.cloma0219@gmail.com>
2022-08-10 09:32:00 -0400

    solution

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  case array.length
+    when 1 then array.join
+    when 2 then array.join(" and ")
+    else
+      oxford_arr = array.slice(0,array.length-1) << "and #{array.slice(array.length-1)}"
+      oxford_arr.join(', ')
+  end
 end
